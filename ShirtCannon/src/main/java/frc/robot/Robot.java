ce1ad928320e96ee276b03f83005cc4a54f493f8
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2019-09-23 18:31:11 -0400

    Initial commit

---
+++ ShirtCannon/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,79 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.DriveTrain;
+
+public class Robot extends TimedRobot {
+  public static DriveTrain drivetrain;
+  public static OI oi;
+
+  Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  @Override
+  public void robotInit() {
+    drivetrain = new DriveTrain();
+    oi = new OI();
+    // chooser.addOption("My Auto", new MyAutoCommand());
+    SmartDashboard.putData("Auto mode", m_chooser);
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_chooser.getSelected();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.start();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
---
+++ ShirtCannon/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.commands.DriveWithJoystick;
+
+/**
+ * @author Christian
+ */
+public class DriveTrain extends Subsystem
+{
+  public DifferentialDrive robotDrive;
+
+  private static final int TalonSRX01ID = 1;
+  private static final int TalonSRX02ID = 2;
+  private static final int TalonSRX03ID = 3;
+  private static final int TalonSRX04ID = 4;
+
+  @Override
+  public void initDefaultCommand()
+  {
+    setDefaultCommand(new DriveWithJoystick());
+  }
+
+  public DriveTrain()
+  {
+    final WPI_TalonSRX leftFront = new WPI_TalonSRX(TalonSRX01ID);
+    final WPI_TalonSRX leftFollower = new WPI_TalonSRX(TalonSRX01ID);
+    final WPI_TalonSRX rightFront = new WPI_TalonSRX(TalonSRX01ID);
+    final WPI_TalonSRX rightFollower = new WPI_TalonSRX(TalonSRX01ID);
+
+    final SpeedControllerGroup rightSide = new SpeedControllerGroup(leftFront, leftFollower);
+    final SpeedControllerGroup leftSide = new SpeedControllerGroup(rightFront, rightFollower);
+
+    robotDrive = new DifferentialDrive(rightSide, leftSide);
+
+    leftFront.configOpenloopRamp(0.3);
+    rightFront.configOpenloopRamp(0.3);
+  }
+
+  public void setDriveSpeeds(double xspeed, double zrotation)
+  {
+    robotDrive.arcadeDrive(xspeed, zrotation);
+  }
+
+  public void stop()
+  {
+    robotDrive.stopMotor();
+  }
+}
